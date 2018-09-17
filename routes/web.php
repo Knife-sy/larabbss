054c79ff806efb69a00dec95130da47a13a51a7a
Knife-sy <13250709552@163.com>
2018-09-17 08:49:01 +0000

    Only authorized user can edit  user profile

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
